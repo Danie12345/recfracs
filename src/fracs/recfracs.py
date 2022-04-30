70aefd37d1bab238ae80a6183e938d34d8fcf7ca
Daniel Malo <danielmaloosorio@gmail.com>
2022-04-30 01:09:15 -0500

    Add main window
    Add screeninfo package, modify requirements.txt
    
    Ignore __pycache__/
    
    Fix name of function

--- src/fracs/recfracs.py
+++ src/fracs/recfracs.py
@@ -1,4 +1,4 @@
 import math
 
-def recfrac(a = 1, err = 10**-3):
+def recfracs(a = 1, err = 10**-3):
     pass
--- src/main.py
+++ src/main.py
@@ -1,3 +1,20 @@
+from screeninfo import get_monitors
 import tkinter as tk
 import fracs.loopfracs as loopfracs
 import fracs.recfracs as recfracs
+
+WIN_WIDTH = 350
+WIN_HEIGHT = 250
+
+root = tk.Tk()
+root.geometry(f"{WIN_WIDTH}x{WIN_HEIGHT}+{(get_monitors()[0].width - WIN_WIDTH)//2}+{(get_monitors()[0].height - WIN_HEIGHT)//2}")
+
+navmenu = tk.Menu(root)
+submenu1 = tk.Menu(navmenu, tearoff=0)
+submenu1.add_command(label='Recursive')
+submenu1.add_command(label='Loops')
+navmenu.add_cascade(label='Algorithm', menu=submenu1)
+
+root.config(menu=navmenu)
+
+root.mainloop()
