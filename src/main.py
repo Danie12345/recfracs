41016a04ec8d7820f69b5c1277073f0926ad870c
Daniel Malo <danielmaloosorio@gmail.com>
2022-05-01 21:21:13 -0500

    Add recfracs logic, update main.py
    Add recrafcs recursive algorithm in recfracs.py.
    
    Add widgets to display menu for algorithm selection, algorithm name, inputs/outputs in main.py.

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import fractions
+from itertools import tee
 from screeninfo import get_monitors
 import tkinter as tk
 import fracs.loopfracs as loopfracs
@@ -9,12 +11,43 @@ WIN_HEIGHT = 250
 root = tk.Tk()
 root.geometry(f"{WIN_WIDTH}x{WIN_HEIGHT}+{(get_monitors()[0].width - WIN_WIDTH)//2}+{(get_monitors()[0].height - WIN_HEIGHT)//2}")
 
-navmenu = tk.Menu(root)
-submenu1 = tk.Menu(navmenu, tearoff=0)
-submenu1.add_command(label='Recursive')
-submenu1.add_command(label='Loops')
-navmenu.add_cascade(label='Algorithm', menu=submenu1)
+algorithms = {'Recursion': recfracs, 'Loops': loopfracs}
+fraction = algorithms['Recursion']
 
-root.config(menu=navmenu)
+
+frame1 = tk.Frame(root)
+algorithmName = tk.StringVar(value = 'Recursion')
+algoritmLabel = tk.Label(frame1, textvariable = algorithmName)
+
+decimal = tk.DoubleVar(value = 1.0)
+decimalInput = tk.Entry(frame1, textvariable = decimal)
+
+numeratorInput = tk.Label(frame1)
+numerator = tk.IntVar()
+
+denominatorInput = tk.Label(frame1)
+denominator = tk.IntVar()
+
+algoritmLabel.pack()
+decimalInput.pack()
+numeratorInput.pack()
+denominatorInput.pack()
+
+
+def switch_algorithm(type):
+  fraction = algorithms[type]
+  algorithmName.set(type)
+
+navmenu = tk.Menu(root)
+algorithm = tk.Menu(navmenu, tearoff=0)
+algorithm.add_command(label = 'Recursive', command = lambda: switch_algorithm('Recursion'))
+algorithm.add_command(label = 'Loops', command = lambda: switch_algorithm('Loops'))
+navmenu.add_cascade(label = 'Algorithm', menu = algorithm)
+
+
+frame1.pack()
+
+
+root.config(menu = navmenu)
 
 root.mainloop()
