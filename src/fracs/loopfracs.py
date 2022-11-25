c71ecb23d3fd3f6e1704fe44646c5b194b3a9aa9
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-11-24 19:32:34 -0600

    Fix precision and step escape conditions

--- src/fracs/loopfracs.py
+++ src/fracs/loopfracs.py
@@ -1,23 +1,52 @@
 import math
+from decimal import Decimal
 
-def loopfracs(a = 1.0, n = 5, err = 10**-4):
+def nEscape(n, i = 0):
+    return i > n
+
+def kEscape(r, k, a):
+    return k > abs(r - Decimal(a)) > -k
+
+def build_frac(ints, decs, a, v=1):
+    v = round(decs[-1])
+    n,d = 1 if v == 1 else 0,1
+    for j in range(len(decs) + 1):
+        u = ints[-j]
+        n += u*d
+        n,d=d,n
+    if a < 0:
+        d *= -1
+    return d,n,Decimal(d)/Decimal(n)
+
+def loopfracs(a = 1.0, n = None, p = None):
+    if n == None and p != None:
+        k = 10**(-p)
+        escape = lambda r = None,  n = None, i = None: kEscape(r, k, a)
+    elif n != None and p == None:
+        k = 10**-14
+        escape = lambda r = None,  n = None, i = None: nEscape(n, i)
+    else:
+        n = n if n != None else 10
+        p = p if p != None else 16
+        k = 10**(-p)
+        escape = lambda r = None,  n = None, i = None: nEscape(n, i) or kEscape(r, k, a)
     ints = [abs(math.trunc(a))]
     decs = [abs(a-math.trunc(a))]
     i = 0
-    while not (-err < decs[-1] < err) and decs[-1] != 0 and i < n:
+    r = 10**100
+    # while not (kEscape(decs[-1], k)) and decs[-1] != 0 and escape(r = r, n = n, i = i):
+    while not escape(r, n, i):
         u = 1/decs[i]
         ints.append(math.trunc(u))
         decs.append(u-math.trunc(u))
+        r = build_frac(ints, decs, a)[2]
+        if abs(Decimal(a) - Decimal(r)) < k:
+            print('breaking from r',abs(Decimal(a) - Decimal(r)), a, r, k)
+            break
         i += 1
-    v = round(decs[-1])
-    n,d = 1 if v == 1 else 0,1
-    for j in range(i + 1):
-        v = round(decs[-j-1])
-        u = ints[-j-1]
-        n += u*d
-        o = n
-        n = d
-        d = o
-    if a < 0:
-        d *= -1
-    return d,n,d/n
+    return build_frac(ints,decs,a,v=round(decs[-1]))
+
+if __name__ == '__main__':
+    a = 1.1
+    b = loopfracs(a, n=3, p = 10)
+    print (b)
